ca0f67f3a4519e9c3737e32ae3988e4bee08e7d9
TeaganRicardo <38710244+TeaganRicardo@users.noreply.github.com>
2026-09-22 21:43:15 +0800

    refactor: keep view snapshots presentation-only

--- Sources/Hades2/Views/Hades2ViewSnapshots.swift
+++ Sources/Hades2/Views/Hades2ViewSnapshots.swift
@@ -39,41 +39,8 @@ struct Hades2ViewConfigSnapshot: Equatable {
     let gameSpeed: Double
 }
 
-struct Hades2ViewInputSnapshot: Equatable {
-    let healthCurrent: String
-    let healthMaximum: String
-    let manaCurrent: String
-    let manaMaximum: String
-    let armorCurrent: String
-    let spellCharge: String
-    let coins: String
-    let materialAmount: String
-    let selectedMaterial: String
-    let rerollAmount: String
-    let multiplier: String
-    let moneyFactor: String
-    let materialFactor: String
-    let gameSpeedInput: String
-}
-
-struct Hades2ViewLockedStatInputSnapshot: Equatable {
-    let grasp: String
-    let dodge: String
-    let crit: String
-    let chargeSpeed: String
-    let moveSpeed: String
-    let sprintSpeed: String
-    let dashSpeed: String
-    let attackSpeed: String
-    let manaRegen: String
-    let enemyDamage: String
-    let enemyHealth: String
-}
-
 struct Hades2ViewCatalogSnapshot: Equatable {
-    let elementInputs: [String: String]
     let materialID: String?
-    let boonRarityFactor: String
     let olympianIDs: [String]
     let pickupIDs: [String]
     let specialIDs: [String]
